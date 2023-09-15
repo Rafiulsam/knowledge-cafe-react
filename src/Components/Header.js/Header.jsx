44d379b287608252521f15056301f1aa365e7a5f
Rafiulsam <rafiulsam69@gmail.com>
2023-09-15 15:03:17 +0600

    added header

--- src/Components/Header.js/Header.jsx
+++ src/Components/Header.js/Header.jsx
@@ -6,7 +6,12 @@ const Header = () => {
         <div className='mx-auto max-w-5xl border-b pb-8 mt-12'>
             <div className='flex justify-between items-center'>
                 <h1 className='font-bold text-4xl'>Knowledge Cafe</h1>
-                <img src={png} alt="" />
+                <div className='flex justify-between items-center gap-10'>
+                    <a href="home">Home</a>
+                    <a href="blog">Blog</a>
+                    <a href="team">Team</a>
+                    <img src={png} alt="" />
+                </div>
             </div>
         </div>
     );
