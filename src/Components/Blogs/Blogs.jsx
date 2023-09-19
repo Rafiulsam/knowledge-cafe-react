8aabb7718278090bd6c4bfaf122bb1d57c799114
Rafiulsam <rafiulsam69@gmail.com>
2023-09-20 05:29:15 +0600

    added alert

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react';
 import Blog from '../Blog/Blog';
 import Bookmarks from '../Bookmarks/Bookmarks';
 import SpentTime from '../SpentTime/SpentTime';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const customId = "custom-id-yes";
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
   const [readTime, setReadTime] = useState([]);
   const [bookmarkedBlogs, setBookmarkedBlogs] = useState(new Set());
-  
+
   useEffect(() => {
     fetch('data.json')
       .then((res) => res.json())
@@ -17,10 +19,20 @@ const Blogs = () => {
   const addReadTime = (time) => {
     setReadTime([...readTime, time]);
   };
-
+  
   const addToBookmark = (blog) => {
+    
     if (bookmarkedBlogs.has(blog)) {
-      alert("This blog is already bookmarked")
+      toast.info('This blog is already bookmarked', {
+        toastId: customId,
+        position: "top-center",
+        autoClose: 1000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: false,
+        theme: "light",
+      });
 
     }
     else {
@@ -47,6 +59,19 @@ const Blogs = () => {
           <Bookmarks bookmarkedBlogs={bookmarkedBlogs}></Bookmarks>
         </div>
       </div>
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        limit={1}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable ={false}
+        pauseOnHover
+        theme="light"
+      />
     </div>
   );
 };
